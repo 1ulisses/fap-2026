c1fb11d413d21f89cda67d6d24c7d218357f0e01
1ulisses <leandrocdefarias@proton.me>
2026-08-06 18:15:31 -0300

    feat: challenges 9-12

--- sql/Projeto_PRF_Inv/sql/investigacao.sql
+++ sql/Projeto_PRF_Inv/sql/investigacao.sql
@@ -11,7 +11,7 @@ FROM read_csv_auto(
 );
 
 -- View e acidente_fatal
---
+
 CREATE OR REPLACE VIEW vw_acidentes_base AS
 SELECT *
 FROM acidentes_prf_2025;
@@ -169,7 +169,7 @@ QUALIFY RANK() OVER (PARTITION BY periodo ORDER BY COUNT(*) DESC) <= 5
 ORDER BY periodo, ranking;
 
 -- DESAFIO 5
--- Neblina é 50% maior que a média nacional.
+-- Neblina (51%) é 50% maior que a média nacional de fatalidade.
 -- Neblina possivelmente diminui a visibilidade causando mais acidentes.
 
 SELECT '================= DESAFIO 5 =================' AS separator;
@@ -296,6 +296,7 @@ LIMIT 15;
 -- possuem baixa cobertura (<1%) mais alta fatalidade:
 -- "Suicídio (presumido)" (55.79%), "Pedestre andava na pista" (41.25%) e
 -- "Entrada inopinada do pedestre" (30.43%)
+-- Acidentes relacionados a pedestres possuem maior fatalidade
 
 SELECT '================= DESAFIO 8 =================' AS separator;
 
@@ -331,5 +332,190 @@ ORDER BY pct_fatal DESC
 LIMIT 15;
 
 -- DESAFIO 9
+-- INTERPRETAÇÃO: Taxa de fatalidade variou entre 6.49% (Jan.) e 8.27% (Mai.)
+-- tendência fraca a aumentar (6x aumentor vs 5x diminuiu)
+-- Possivelmente por motivos sazonais ou eventos
 
 SELECT '================= DESAFIO 9 =================' AS separator;
+
+WITH mes_stats AS (
+    SELECT
+        extract(MONTH FROM cast(data_inversa AS DATE)) AS mes,
+        count(*) AS total_acidentes,
+        sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) AS fatais,
+        round(
+            100.0 * sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) / count(*), 2
+        ) AS pct_fatal
+    FROM
+        vw_acidentes_base
+    GROUP BY extract(MONTH FROM cast(data_inversa AS DATE))
+)
+
+SELECT
+    mes,
+    total_acidentes,
+    fatais,
+    pct_fatal,
+    lag(pct_fatal) OVER (ORDER BY mes) AS pct_fatal_mes_anterior,
+    CASE
+        WHEN pct_fatal > lag(pct_fatal) OVER (ORDER BY mes) THEN 'AUMENTOU'
+        WHEN pct_fatal < lag(pct_fatal) OVER (ORDER BY mes) THEN 'DIMINUIU'
+        ELSE 'ESTÁVEL'
+    END AS tendencia
+FROM mes_stats
+ORDER BY mes;
+
+-- DESAFIO 10
+-- INTERPRETAÇÃO: Tipos de acidente com maior lift são: Atropelamento(4.1),
+-- Colisão frontal(4.1) e Colisão lateral sentido oposto(1.37)
+-- Tipos com menor lift são: Incêndio(0.0), Engavetamento(0.31) e
+-- Queda de ocupante do veículo(0.35).
+-- Acidentes envolvendo pedestres e acidentes violentos são mais fatais que
+-- o contrário.
+
+SELECT '================= DESAFIO 10 =================' AS separator;
+
+WITH global_rate AS (
+    SELECT
+        round(
+            100.0 * sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) / count(*), 3
+        ) AS taxa_global
+    FROM
+        vw_acidentes_base
+),
+
+tipo_stats AS (
+    SELECT
+        tipo_acidente,
+        count(*) AS total_acidentes,
+        sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) AS fatais,
+        round(
+            100.0 * sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) / count(*), 3
+        ) AS taxa_segmento
+    FROM
+        vw_acidentes_base
+    GROUP BY tipo_acidente
+    HAVING count(*) >= 100
+)
+
+SELECT
+    tipo_acidente,
+    total_acidentes,
+    taxa_segmento,
+    (SELECT taxa_global FROM global_rate) AS taxa_global,
+    round(taxa_segmento / (SELECT taxa_global FROM global_rate), 3) AS lift,
+    CASE
+        WHEN
+            taxa_segmento / (SELECT taxa_global FROM global_rate) > 2
+            THEN 'MUITO ALTO'
+        WHEN
+            taxa_segmento / (SELECT taxa_global FROM global_rate) > 1.5
+            THEN 'ALTO'
+        WHEN
+            taxa_segmento / (SELECT taxa_global FROM global_rate) > 1
+            THEN 'ACIMA MÉDIA'
+        ELSE 'ABAIXO MÉDIA'
+    END AS classificacao_risco
+FROM tipo_stats
+ORDER BY lift DESC;
+
+-- DESAFIO 11
+-- INTERPRETAÇÃO: Amanhecer, Simples e Céu Claro(15.69%),
+-- Plena noite, Simples, Ignorado(13.61%) e Céu claro(13.45%)
+-- são as 3 combinações mais fatais.
+-- Plena noite, Simples e Céu claro possui maior volume 11.52%.
+-- Plena noite domina ranking possivelmente devido a baixa visibilidade e fadiga
+
+SELECT '================= DESAFIO 11 =================' AS separator;
+
+WITH perfil_risco AS (
+    SELECT
+        fase_dia,
+        tipo_pista,
+        condicao_metereologica,
+        count(*) AS total_acidentes,
+        sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) AS fatais,
+        round(
+            100.0 * sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) / count(*), 2
+        ) AS pct_fatal
+    FROM
+        vw_acidentes_base
+    GROUP BY fase_dia, tipo_pista, condicao_metereologica
+    HAVING count(*) >= 200
+)
+
+SELECT
+    fase_dia,
+    tipo_pista,
+    condicao_metereologica,
+    total_acidentes,
+    fatais,
+    pct_fatal,
+    round(100.0 * total_acidentes / sum(total_acidentes) OVER (), 2)
+        AS pct_base_total,
+    rank() OVER (ORDER BY pct_fatal DESC) AS ranking
+FROM perfil_risco
+WHERE pct_fatal > 7.18
+ORDER BY pct_fatal DESC
+LIMIT 10;
+
+-- DESAFIO 12
+-- HIPÓTESE: Acidentes a noite em pistas simples são mais fatais que outras
+-- combinações, devido a baixa visibilidade, separação física das ruas e fadiga
+-- do condutor, possivelmente após a jornada de trabalho.
+-- RESULTADO: Noite e Pista simples possui 14564 acidentes e 1899 fatais com 13%
+-- de porcentagem de fatalidade.
+-- CONCLUSÃO: Confirmado
+
+SELECT '================= DESAFIO 12 =================' AS separator;
+
+WITH hipotese_teste AS (
+    SELECT
+        CASE
+            WHEN
+                (
+                    cast(extract(HOUR FROM cast(horario AS TIME)) AS INTEGER)
+                    >= 18
+                    OR cast(extract(HOUR FROM cast(horario AS TIME)) AS INTEGER)
+                    <= 5
+                )
+                AND tipo_pista = 'Simples'
+                THEN 'Noite_Pista_Simples'
+            ELSE 'Outros'
+        END AS grupo,
+        count(*) AS total_acidentes,
+        sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) AS fatais,
+        round(
+            100.0 * sum(CASE WHEN mortos >= 1 THEN 1 ELSE 0 END) / count(*), 2
+        ) AS pct_fatal
+    FROM
+        read_csv_auto(
+            'dados_brutos/acidentes2025.csv',
+            delim = ';',
+            header = true,
+            encoding = 'latin-1'
+        )
+    GROUP BY
+        CASE
+            WHEN
+                (
+                    cast(extract(HOUR FROM cast(horario AS TIME)) AS INTEGER)
+                    >= 18
+                    OR cast(extract(HOUR FROM cast(horario AS TIME)) AS INTEGER)
+                    <= 5
+                )
+                AND tipo_pista = 'Simples'
+                THEN 'Noite_Pista_Simples'
+            ELSE 'Outros'
+        END
+)
+
+SELECT
+    grupo,
+    total_acidentes,
+    fatais,
+    pct_fatal,
+    round(pct_fatal / lag(pct_fatal) OVER (ORDER BY pct_fatal), 2)
+        AS vezes_maior
+FROM hipotese_teste
+ORDER BY pct_fatal DESC;
